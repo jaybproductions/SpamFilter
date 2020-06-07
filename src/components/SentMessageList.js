66e920f0203b5eab25db3f5c2323ce47fd67d5b7
JayB Productions <jaybgameproductions@gmail.com>
2020-06-07 19:51:57 -0400

    FINALLY!!!

--- src/components/SentMessageList.js
+++ src/components/SentMessageList.js
@@ -2,7 +2,6 @@ import React from "react";
 import { IonList, IonItem, IonButton } from "@ionic/react";
 
 const SentMessageList = ({ email, message, index, isBlocked }) => {
-  function sendBlockedEmail() {}
   return (
     <IonList>
       <IonItem>{email}</IonItem>
